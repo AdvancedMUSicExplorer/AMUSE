026dafbabe9b70b42c7b412a6fc2bdca047be75a
waeltken <waeltken@a1842f4f-d49d-467b-b3b9-0e97a6866e28>
2010-08-04 14:23:41 +0000

    ++Minor changes

--- amuse/amuseGUI.bat
+++ amuse/amuseGUI.bat
@@ -1,4 +1,4 @@
-export AMUSEHOME=C:\Java\amuse
+set AMUSEHOME=C:\Java\amuse
 
 java -classpath lib\amuse-gui.jar;lib\amuse-frame.jar;lib\amuse-utils.jar;config\node\extractor\extractorNode.jar;config\node\processor\processorNode.jar;config\node\trainer\trainerNode.jar;config\node\classifier\classifierNode.jar;config\node\validator\validatorNode.jar;config\node\optimizer\optimizerNode.jar;lib\miglayout-3.7-swing.jar;lib\weka.jar;lib\log4j-1.2.14.jar;lib\yale.jar;lib\xpp3.jar;lib\joone-engine.jar;lib\colt.jar;lib\xstream.jar;lib\rapidminer.jar;lib\tritonus_remaining-0-1.3.6.jar;lib\tritonus_share-0.3.6.jar;lib\jl1.0.jar amuse.scheduler.gui.controller.WizardController
  
