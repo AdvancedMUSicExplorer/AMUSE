3347ce67a6aec8225e3513f8f892914f07cea0e1
Igor Vatolkin <46521894+igorvatolkin@users.noreply.github.com>
2010-08-05 14:59:15 +0000

    Path to jama.jar is provided

--- amuse/amuseGUI.sh
+++ amuse/amuseGUI.sh
@@ -2,6 +2,6 @@
 
 export AMUSEHOME=/home/workspace/amuse
 
-java -classpath lib/amuse-gui.jar:lib/amuse-frame.jar:lib/amuse-utils.jar:config/node/extractor/extractorNode.jar:config/node/processor/processorNode.jar:config/node/trainer/trainerNode.jar:config/node/classifier/classifierNode.jar:config/node/validator/validatorNode.jar:config/node/optimizer/optimizerNode.jar:lib/miglayout-3.7-swing.jar:lib/weka.jar:lib/log4j-1.2.14.jar:lib/yale.jar:lib/xpp3.jar:lib/joone-engine.jar:lib/colt.jar:lib/xstream.jar:lib/rapidminer.jar:lib/tritonus_remaining-0-1.3.6.jar:lib/tritonus_share-0.3.6.jar:lib/jl1.0.jar amuse.scheduler.gui.controller.WizardController
+java -classpath lib/amuse-gui.jar:lib/amuse-frame.jar:lib/amuse-utils.jar:config/node/extractor/extractorNode.jar:config/node/processor/processorNode.jar:config/node/trainer/trainerNode.jar:config/node/classifier/classifierNode.jar:config/node/validator/validatorNode.jar:config/node/optimizer/optimizerNode.jar:lib/miglayout-3.7-swing.jar:lib/weka.jar:lib/log4j-1.2.14.jar:lib/yale.jar:lib/xpp3.jar:lib/joone-engine.jar:lib/colt.jar:lib/xstream.jar:lib/rapidminer.jar:lib/tritonus_remaining-0-1.3.6.jar:lib/tritonus_share-0.3.6.jar:lib/jl1.0.jar:tools/RapidMiner4.5/lib/jama.jar amuse.scheduler.gui.controller.WizardController
 
  
