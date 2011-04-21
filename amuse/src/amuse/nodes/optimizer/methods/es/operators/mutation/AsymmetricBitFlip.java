a9123cd102e4685c006c85b2007ca4fb94438316
Igor Vatolkin <46521894+igorvatolkin@users.noreply.github.com>
2011-04-21 09:17:29 +0000

    Asymmetric mutation supports the allowed maximum of selected features

--- amuse/src/amuse/nodes/optimizer/methods/es/operators/mutation/AsymmetricBitFlip.java
+++ amuse/src/amuse/nodes/optimizer/methods/es/operators/mutation/AsymmetricBitFlip.java
@@ -23,6 +23,7 @@
  */
 package amuse.nodes.optimizer.methods.es.operators.mutation;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 import org.apache.log4j.Level;
@@ -46,6 +47,7 @@ public class AsymmetricBitFlip extends RandomBitFlip {
 	/** Parameters from ESConfiguration which are saved here for faster processing */
 	double p_01;
 	double p_10;
+	double maxRateOfSelectedFeatures = 1.0;
 	
 	/*
 	 * (non-Javadoc)
@@ -53,6 +55,10 @@ public class AsymmetricBitFlip extends RandomBitFlip {
 	 */
 	public void mutate(RepresentationInterface representation) throws NodeException {
 		AmuseLogger.write(this.getClass().getName(), Level.DEBUG, "Asymmetric bit flip mutation started");
+		
+		// Save the added features if some of them must be removed due to limit of maximum feature number
+		ArrayList<Integer> addedFeatures = new ArrayList<Integer>();
+		int numberOfSelectedFeatures = 0;
 		if(representation instanceof BinaryVector) {
 			BinaryVector valueToMutate = (BinaryVector)representation;
 			Random rand = new Random();
@@ -65,29 +71,51 @@ public class AsymmetricBitFlip extends RandomBitFlip {
 					double mutationProbability = (p_01*gamma*this.selfAdaptationFactor) / valueToMutate.getValue().length;
 					if(rand.nextDouble() < mutationProbability) {
 						valueToMutate.getValue()[i] = true;
+						numberOfSelectedFeatures++;
+						addedFeatures.add(i);
 					}
 				}
 				
 				// Perform 1 -> 0 mutation?
 				else {
+					numberOfSelectedFeatures++;
 					double mutationProbability = (p_10*gamma*this.selfAdaptationFactor) / valueToMutate.getValue().length;
 					if(rand.nextDouble() < mutationProbability) {
 						valueToMutate.getValue()[i] = false;
+						numberOfSelectedFeatures--;
+					}
+				}
+			}
+			
+			// Check if the number of selected features is lower than the allowed maximum
+			int maxAllowed = new Double(valueToMutate.getValue().length * this.maxRateOfSelectedFeatures).intValue();
+			if(numberOfSelectedFeatures > maxAllowed) {
+				
+				// Reduce the number of selected features removing at first the features which have been added during
+				// the last mutation step
+				for(int i=0;i<numberOfSelectedFeatures-maxAllowed;i++) {
+					if(!addedFeatures.isEmpty()) {
+						int f = rand.nextInt(addedFeatures.size());
+						valueToMutate.getValue()[addedFeatures.get(f)] = false;
+						addedFeatures.remove(f);
+					} else {
+						boolean isRemoved = false;
+						while(!isRemoved) {
+							int f = rand.nextInt(valueToMutate.getValue().length);
+							if(valueToMutate.getValue()[f]) {
+								valueToMutate.getValue()[f] = false;
+								isRemoved = true;
+							}
+						}
 					}
 				}
 			}
 			
 			// Check if the new individual has at least one feature; otherwise add a random feature!
-			boolean added = false;
-			for(int i=0;i<valueToMutate.getValue().length;i++) {
-				if(valueToMutate.getValue()[i] == true) {
-					added = true;
-					break;
-				}
-			}
-			if(!added) {
+			if(numberOfSelectedFeatures == 0) {
 				valueToMutate.getValue()[rand.nextInt(valueToMutate.getValue().length)] = true;
 			}
+			
 			AmuseLogger.write(this.getClass().getName(), Level.DEBUG, "Mutated value: " + valueToMutate.toString());
 		} else {
 			throw new NodeException("Representation class (" + representation.getClass().toString() + ") must be BinaryVector!");
@@ -108,6 +136,8 @@ public class AsymmetricBitFlip extends RandomBitFlip {
 					p_01 = new Double(parameters.item(i).getAttributes().getNamedItem("doubleValue").getNodeValue());
 				} else if(parameterName.equals(new String("p_10"))) {
 					p_10 = new Double(parameters.item(i).getAttributes().getNamedItem("doubleValue").getNodeValue());
+				} else if(parameterName.equals(new String("maxRateOfSelectedFeatures"))) {
+					maxRateOfSelectedFeatures = new Double(parameters.item(i).getAttributes().getNamedItem("doubleValue").getNodeValue());
 				}
 			}
 		}
