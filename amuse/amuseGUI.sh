949f78af840879521b73b17eee892fb14e49af06
frederik-h <frederik-h@a1842f4f-d49d-467b-b3b9-0e97a6866e28>
2017-10-18 09:51:23 +0000

    UPDATE: updated the script amuseGUI.sh, revised code for readability and updated the libs needed to run amuseGUI.sh.

--- amuse/amuseGUI.sh
+++ amuse/amuseGUI.sh
@@ -2,6 +2,4 @@
 
 export AMUSEHOME=/home/workspace/amuse
 
-java -classpath lib/amuse-gui.jar:lib/amuse-frame.jar:lib/amuse-utils.jar:config/node/extractor/extractorNode.jar:config/node/processor/processorNode.jar:config/node/trainer/trainerNode.jar:config/node/classifier/classifierNode.jar:config/node/validator/validatorNode.jar:config/node/optimizer/optimizerNode.jar:lib/miglayout-3.7-swing.jar:lib/weka.jar:lib/log4j-1.2.14.jar:lib/yale.jar:lib/xpp3.jar:lib/joone-engine.jar:lib/colt.jar:lib/xstream.jar:lib/rapidminer.jar:lib/tritonus_remaining-0-1.3.6.jar:lib/tritonus_share-0.3.6.jar:lib/jl1.0.jar:tools/RapidMiner4.5/lib/jama.jar amuse.scheduler.gui.controller.WizardController
-
- 
+java -classpath lib/amuse-gui.jar:lib/amuse-frame.jar:lib/amuse-utils.jar:config/node/extractor/extractorNode.jar:config/node/processor/processorNode.jar:config/node/trainer/trainerNode.jar:config/node/classifier/classifierNode.jar:config/node/validator/validatorNode.jar:config/node/optimizer/optimizerNode.jar:lib/jl1.0.jar:lib/launcher.jar:lib/log4j-1.2.14.jar:lib/miglayout-3.7-swing.jar:lib/rapidminer.jar:lib/tritonus_share-0.3.6.jar:lib/vldocking.jar:lib/weka.jar:lib/xpp3.jar:lib/xstream.jar:lib/mp3plugin.jar:lib/tritonus_remaining-0.3.6.jar amuse.scheduler.gui.controller.WizardController
