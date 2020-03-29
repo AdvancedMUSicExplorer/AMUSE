bb4ff2b612f3ae5bddc0d65f24a4b4eef81e2841
philippginsel <philipp.ginsel@tu-dortmund.de>
2020-03-29 12:37:29 +0200

    BUGFIX: fixed a bug where the adapter class of the raw feature processing method was specified falsely in the processorConversionAlgorithmTable UPDATE: added standard comment to the new classes

--- amuse/src/amuse/data/InputFeatureType.java
+++ amuse/src/amuse/data/InputFeatureType.java
@@ -1,5 +1,33 @@
+/** 
+ * This file is part of AMUSE framework (Advanced MUsic Explorer).
+ * 
+ * Copyright 2006-2020 by code authors
+ * 
+ * Created at TU Dortmund, Chair of Algorithm Engineering
+ * (Contact: <http://ls11-www.cs.tu-dortmund.de>) 
+ *
+ * AMUSE is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * AMUSE is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with AMUSE. If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * Creation date: 23.03.2020
+ */
 package amuse.data;
 
+/**
+ * Defines the Input Feature Type. Can be either
+ * - raw unprocessed features from the Amuse feature database
+ * - processed features from the Amuse processed feature database
+ */
 public enum InputFeatureType {
 	RAW_FEATURES,
 	PROCESSED_FEATURES;
--- amuse/src/amuse/nodes/processor/methods/converters/RawFeaturesConverter.java
+++ amuse/src/amuse/nodes/processor/methods/converters/RawFeaturesConverter.java
@@ -1,3 +1,26 @@
+/** 
+ * This file is part of AMUSE framework (Advanced MUsic Explorer).
+ * 
+ * Copyright 2006-2020 by code authors
+ * 
+ * Created at TU Dortmund, Chair of Algorithm Engineering
+ * (Contact: <http://ls11-www.cs.tu-dortmund.de>) 
+ *
+ * AMUSE is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * AMUSE is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with AMUSE. If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * Creation date: 23.03.2020
+ */
 package amuse.nodes.processor.methods.converters;
 
 import java.util.ArrayList;
@@ -11,6 +34,12 @@ import amuse.nodes.processor.ProcessorNodeScheduler;
 import amuse.nodes.processor.interfaces.MatrixToVectorConverterInterface;
 import amuse.util.AmuseLogger;
 
+/**
+ * Harmonizes raw features without calculating any statisitcs on them
+ * 
+ * @author Philipp Ginsel
+ * @version $Id$
+ */
 public class RawFeaturesConverter extends AmuseTask implements MatrixToVectorConverterInterface{
 
 	@Override
