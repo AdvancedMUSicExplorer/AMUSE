bfafaf9e2820a78248b671ab0c2c3601577aad50
clarapingel <cpingel@riseup.net>
2023-11-13 13:07:29 +0100

    UPDATE reduced modalities

--- amuse/src/amuse/nodes/extractor/modality/AudioModality.java
+++ amuse/src/amuse/nodes/extractor/modality/AudioModality.java
@@ -1,5 +1,58 @@
 package amuse.nodes.extractor.modality;
 
-public interface AudioModality extends Modality {
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+import javax.sound.sampled.AudioFileFormat;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.UnsupportedAudioFileException;
+
+public class AudioModality implements Modality {
+	
+	public enum AudioFormat{MP3, WAVE, AIFF, AIFC, SND, AU}
+	
+	private List<AudioFormat> formats;
+	
+	public AudioModality(List<AudioFormat> formats) {
+		this.formats = formats;
+	}
+	
+	@Override
+	public List getFormats() {
+		return formats;
+	}
+
+	@Override
+	public boolean matchesRequirements(File file) {
+		/*Check file format*/
+		AudioFileFormat audioFileFormat;
+		try {
+			audioFileFormat = AudioSystem.getAudioFileFormat(file);
+			if(audioFileFormat.getType() != AudioFileFormat.Type.WAVE && formats.contains(AudioFormat.WAVE)) {
+				return true;
+			}
+			else if(audioFileFormat.getType() != AudioFileFormat.Type.SND && formats.contains(AudioFormat.SND)) {
+				return true;
+			}
+			else if(audioFileFormat.getType() != AudioFileFormat.Type.AIFF && formats.contains(AudioFormat.AIFF)) {
+				return true;
+			}
+			else if(audioFileFormat.getType() != AudioFileFormat.Type.AIFC && formats.contains(AudioFormat.AIFC)) {
+				return true;
+			}
+			else if(audioFileFormat.getType() != AudioFileFormat.Type.AU && formats.contains(AudioFormat.AU)) {
+				return true;
+			}
+			else if(file.getPath().endsWith(".mp3") && formats.contains(AudioFormat.MP3)) {
+				return true;
+			}
+		} catch (UnsupportedAudioFileException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return false;
+	}
 
 }
--- amuse/src/amuse/nodes/extractor/modality/SymbolicModality.java
+++ amuse/src/amuse/nodes/extractor/modality/SymbolicModality.java
@@ -1,5 +1,35 @@
 package amuse.nodes.extractor.modality;
 
-public interface SymbolicModality extends Modality {
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+import javax.sound.midi.InvalidMidiDataException;
+import javax.sound.midi.MidiFileFormat;
+import javax.sound.midi.MidiSystem;
+
+public class SymbolicModality implements Modality {
+
+	public enum SymbolicFormat{MIDI, MUSICXML}
+	
+	private List<SymbolicFormat> formats;
+	
+	@Override
+	public List<SymbolicFormat> getFormats() {
+		return formats;
+	}
+
+	@Override
+	public boolean matchesRequirements(File file) {
+		/*Check, if file is appropriate midi data*/
+		try {
+			MidiFileFormat midifileformat = MidiSystem.getMidiFileFormat(file);
+		} catch (InvalidMidiDataException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return false;
+	}
 	
 }
